d5608fe2f338a7db7cffa7750785a91be8c622ec
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-21 13:26:51 -0500

    foreach loop written

--- csv_to_ical.ps1
+++ csv_to_ical.ps1
@@ -1,3 +1,14 @@
 $csvContent = Get-Content .\data.csv
 $table = $csvContent|ConvertFrom-Csv
-$table
+$calendars = $table.TYPE|Sort-Object -Unique
+$calendars | ForEach-Object {
+    $calendar = $_
+    
+    $events = $table|Where-Object -Property TYPE -eq $calendar
+    # $events | ConvertTo-Csv | Set-Content "$calendar.csv"
+    "$calendar.ics"
+    $events |Format-Table
+}
+
+
+# CONSIDERING USING https://justinbraun.com/2018/01/powershell-dynamic-generation-of-an-ical-vcalendar-ics-format-file/
