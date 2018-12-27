8126fa52e26ddb63df75518403723d2cd400379b
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-27 15:02:13 -0500

    Better output

--- csv_to_ical.ps1
+++ csv_to_ical.ps1
@@ -9,7 +9,7 @@ $calendars = $table | Group-Object -property 'TYPE'
 # Goes through list again, once per event type
 $calendars | ForEach-Object {
     $calendar = $_
-    $calendarName = $calendar.Name
+    $calendarName = "$($calendar.Name) (Updated $((get-date).ToString('yyyy-MM-dd')))"
     # find relevant eventEntries in the table
     $eventEntries = @()
     $events = $calendar.Group
@@ -28,18 +28,35 @@ $calendars | ForEach-Object {
         }
     }
     # and this writes the file out
-    # $eventEntries | ConvertTo-iCal -calendar $calendarName|Set-Content ".\results\$calendarName.ics"
-    # Start-Process ".\results\$calendarName.ics" # uncomment this line to open each file on creation
+    $ical = $eventEntries | ConvertTo-iCal -calendar $calendarName 
+    $ical | Set-Content ".\results\$calendarName.ics"
+    #".\results\$calendarName.ics"
+    Start-Process ".\results\$calendarName.ics" # uncomment this line to open each file on creation
     # $eventEntries | Add-OutlookEvent
     
 }
+
+
+
+# $args = @{
+#     Summary     = "sum"
+#     Description = "desc"
+#     Start       = (get-date).AddHours(1)
+#     End         = (get-date).AddHours(2)
+#     Reminder = $True
+#     ReminderDelta = "00:59:00"
+# }
+# $CalendarEvent = New-Object "CalendarEvent" -Property $args
+
+# $CalendarEvent| ConvertTo-iCal|set-content test.ics
+# Start-Process test.ics
 
 # Useful links:
 # partially using https://justinbraun.com/2018/01/powershell-dynamic-generation-of-an-ical-vcalendar-ics-format-file/
 # https://stackoverflow.com/questions/35645402/how-to-specify-timezone-in-ics-file-which-will-work-efficiently-with-google-outl
 # https://apps.marudot.com/ical/
 # AND TO IMPORT https://thescriptkeeper.wordpress.com/2013/09/27/import-a-bunch-of-ics-calendar-files-with-powershell/
-# But at this point we can just create new library items based on the above criteria, and we can skip the ICS stuff really
+# We then use this knowledge to just inject into outlook
 
 # SharePoint Shared Calendar (another direciotn we can take this project)
 # stssync://sts/?ver=1.1&type=calendar&cmd=add-folder&base-url=https%3A%2F%2Fattentiem%2Esharepoint%2Ecom%2Fsites%2Fusac%2Emmm&list-url=%2FLists%2FBiWeekly%2520Payroll%2F&guid=%7B9cd4bb9e%2Df405%2D40f8%2D9727%2D614ea89a15b1%7D&site-name=Attenti%20Electronic%20Monitoring&list-name=Bi%2DWeekly%20Payroll
