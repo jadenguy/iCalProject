bd7178605c47261098fa84951a3b358cbed36642
Alfredo Inostroza <jadenguy@gmail.com>
2020-02-20 19:16:28 -0500

    move src file out

--- process.ps1
+++ process.ps1
@@ -1,12 +1,12 @@
-. $PSScriptRoot\CalendarEvent.ps1
-. $PSScriptRoot\Get-BusinessDay.ps1
-. $PSScriptRoot\ConvertTo-iCal.ps1
-. $PSScriptRoot\Add-OutlookEvent.ps1
-$global:holidays = (Import-Csv  $PSScriptRoot\holiday.csv ).date | ForEach-Object { get-date $_ }
+. $PSScriptRoot\src\CalendarEvent.ps1
+. $PSScriptRoot\src\Get-BusinessDay.ps1
+. $PSScriptRoot\src\ConvertTo-iCal.ps1
+. $PSScriptRoot\src\Add-OutlookEvent.ps1
+$global:holidays = (Import-Csv  $PSScriptRoot\src\holiday.csv ).date | ForEach-Object { get-date $_ }
 
 # Gets eventEntries table
 $resultsFolder = new-item -Path $PSScriptRoot -Name results -ItemType Directory -Force
-$table = Get-Content  $PSScriptRoot\events.csv | ConvertFrom-Csv
+$table = Get-Content  $PSScriptRoot\src\events.csv | ConvertFrom-Csv
 # Creates list of each type of heading in eventEntries table
 $calendars = $table | Group-Object -property 'TYPE'
 # Goes through list again, once per event type
@@ -20,7 +20,7 @@ $calendars | ForEach-Object {
         $event = $_
         $args = @{
             Summary     = $event.TYPE
-            Description = Get-Content $PSScriptRoot\body.md -raw
+            Description = Get-Content $PSScriptRoot\src\body.md -raw
             Location    = "707 W Lutz Lake Fern Rd, Lutz, FL 33548"
             Start       = (get-date $event.DATE).AddHours(7.5)
             End         = (get-date $event.DATE).AddHours(17)
@@ -36,7 +36,7 @@ $calendars | ForEach-Object {
     $ical = $eventEntries | ConvertTo-iCal -calendar $calendarName 
     $ical | Set-Content $icalPath
     #".\results\$calendarName.ics"
-    Start-Process $icalPath # uncomment this line to open each file on creation
+    # Start-Process $icalPath # uncomment this line to open each file on creation
     # $eventEntries | Add-OutlookEvent
     
 }
@@ -62,6 +62,3 @@ $calendars | ForEach-Object {
 # https://apps.marudot.com/ical/
 # AND TO IMPORT https://thescriptkeeper.wordpress.com/2013/09/27/import-a-bunch-of-ics-calendar-files-with-powershell/
 # We then use this knowledge to just inject into outlook
-
-# SharePoint Shared Calendar (another direciotn we can take this project)
-# stssync://sts/?ver=1.1&type=calendar&cmd=add-folder&base-url=https%3A%2F%2Fattentiem%2Esharepoint%2Ecom%2Fsites%2Fusac%2Emmm&list-url=%2FLists%2FBiWeekly%2520Payroll%2F&guid=%7B9cd4bb9e%2Df405%2D40f8%2D9727%2D614ea89a15b1%7D&site-name=Attenti%20Electronic%20Monitoring&list-name=Bi%2DWeekly%20Payroll
